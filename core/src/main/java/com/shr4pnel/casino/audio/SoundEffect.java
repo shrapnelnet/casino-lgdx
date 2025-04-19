31f836fa9fd8a1882cf73e1215b120dc693c9247
observer <admin@shr4pnel.com>
2025-04-19 19:19:03 +0100

    add method play and overload to play sound effects 1mb<

--- core/src/main/java/com/shr4pnel/casino/audio/SoundEffect.java
+++ core/src/main/java/com/shr4pnel/casino/audio/SoundEffect.java
@@ -1,4 +1,16 @@
 package com.shr4pnel.casino.audio;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class SoundEffect {
+    public static void play(String path) {
+        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        sound.play(1.0f);
+    }
+
+    public static void play(String path, float volume) {
+        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        sound.play(volume);
+    }
 }
