3c3d13c709000e71976c1eb51e307faa36f5197d
observer <admin@shr4pnel.com>
2025-04-19 18:05:18 +0100

    add scenemanager to handle the games state

---
+++ core/src/main/java/com/shr4pnel/casino/scene/SceneManager.java
@@ -0,0 +1,47 @@
+package com.shr4pnel.casino.scene;
+
+import com.strongjoshua.console.Console;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SceneManager {
+    private static scene activeScene = scene.INTRO;
+    private static final Map<scene, String> sceneToPrettyName;
+    private static final Map<String, scene> prettyNameToScene;
+
+    static {
+        sceneToPrettyName = new HashMap<>();
+        sceneToPrettyName.put(scene.INTRO, "Intro");
+        sceneToPrettyName.put(scene.BLACKJACK, "Blackjack");
+        sceneToPrettyName.put(scene.POKER, "Poker");
+
+        prettyNameToScene = new HashMap<>();
+        prettyNameToScene.put("intro", scene.INTRO);
+        prettyNameToScene.put("blackjack", scene.BLACKJACK);
+        prettyNameToScene.put("poker", scene.POKER);
+    }
+
+    public static scene getActiveScene() {
+        return activeScene;
+    }
+
+    public static void setActiveScene(scene activeScene) {
+        SceneManager.activeScene = activeScene;
+    }
+
+    public static String getActiveScenePretty() {
+        return sceneToPrettyName.get(activeScene);
+    }
+
+    public static scene getSceneAsEnum(String prettyName) {
+        prettyName = prettyName.toLowerCase();
+        return prettyNameToScene.get(prettyName);
+    }
+
+    public enum scene {
+        INTRO,
+        BLACKJACK,
+        POKER
+    }
+}
