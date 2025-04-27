a6ed6f5477eb01d537c6b29f1f583bb0bac49079
observer <admin@shr4pnel.com>
2025-04-27 15:45:11 +0100

    further abstract away sound effect management (less LoC in main)

--- core/src/main/java/com/shr4pnel/casino/audio/SoundEffect.java
+++ core/src/main/java/com/shr4pnel/casino/audio/SoundEffect.java
@@ -1,4 +1,35 @@
 package com.shr4pnel.casino.audio;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class SoundEffect {
+    private long id;
+    private String path;
+    private float vol;
+    private final Sound soundManager = Gdx.audio.newSound(Gdx.files.internal("sound/load.ogg"));
+
+    public SoundEffect(String path) {
+        this.path = path;
+    }
+
+    public SoundEffect(String path, float vol) {
+        this.path = path;
+        this.vol = vol;
+    }
+
+    public void play() {
+        if (path == null)
+            return;
+        id = SoundEffectHelper.playWithID(path, vol);
+    }
+
+    public void cancel() {
+        soundManager.stop(id);
+    }
+
+    public void dispose() {
+        cancel();
+        soundManager.dispose();
+    }
 }
--- core/src/main/java/com/shr4pnel/casino/audio/SoundEffectHelper.java
+++ core/src/main/java/com/shr4pnel/casino/audio/SoundEffectHelper.java
@@ -2,30 +2,110 @@ package com.shr4pnel.casino.audio;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.files.FileHandle;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SoundEffectHelper {
+    private static final Map<String, Sound> loadedSounds = new HashMap<>();
+    /**
+     * Plays a sound at full volume
+     * @param path Path to audio file, relative to asset directory
+     */
     public static void play(String path) {
-        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        Sound sound = loadedSounds.get(path);
+        if (sound != null) {
+            sound.play();
+            return;
+        }
+        Gdx.app.log("SoundEffectHelper", "Loading audio file " + path + ", which was not preloaded");
+        sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        loadedSounds.put(path, sound);
         sound.play(1.0f);
     }
 
+    /**
+     * Plays a sound at a specified volume
+     * @param path Path to audio file, relative to asset directory
+     * @param volume Volume to play sound at, from 0-1
+     */
     public static void play(String path, float volume) {
-        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        Sound sound = loadedSounds.get(path);
+        if (sound != null) {
+            sound.play(volume);
+            return;
+        }
+        Gdx.app.log("SoundEffectHelper", "Loading audio file " + path + ", which was not preloaded");
+        sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        loadedSounds.put(path, sound);
         sound.play(volume);
     }
 
+    /**
+     * Plays a sound, retaining the sound ID
+     * @param path Path to audio file, relative to asset directory
+     * @return Sound ID
+     */
     public static long playWithID(String path) {
-        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        Sound sound = loadedSounds.get(path);
+        if (sound != null) {
+            return sound.play();
+        }
+        Gdx.app.log("SoundEffectHelper", "Loading audio file " + path + ", which was not preloaded");
+        sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        loadedSounds.put(path, sound);
         return sound.play(1.0f);
     }
 
+    /**
+     * Plays a sound, retaining the sound ID at a specified volume
+     * @param path Path to audio file, relative to asset directory
+     * @param volume Volume to play sound at, from 0-1
+     * @return Sound ID
+     */
     public static long playWithID(String path, float volume) {
-        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        Sound sound = loadedSounds.get(path);
+        if (sound != null) {
+            return sound.play(volume);
+        }
+        Gdx.app.log("SoundEffectHelper", "Loading audio file " + path + ", which was not preloaded");
+        sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        loadedSounds.put(path, sound);
         return sound.play(volume);
     }
 
-    public static void cancel(long id) {
-        Sound sound = Gdx.audio.newSound(Gdx.files.internal("sound/load.ogg"));
-        sound.stop(id);
+    /**
+     * Builds a sound effect instance
+     * @param path Path to audio file, relative to asset directory
+     * @return A SoundEffect object, with builtin play and cancel methods to avoid manual management of ID
+     */
+    public static SoundEffect build(String path) {
+        return new SoundEffect(path);
+    }
+
+    /**
+     * Builds a sound effect instance, specifying a volume
+     * @param path Path to audio file, relative to asset directory
+     * @return A SoundEffect object, with builtin play and cancel methods to avoid manual management of ID
+     */
+    public static SoundEffect build(String path, float vol) {
+        return new SoundEffect(path, vol);
+    }
+
+    public static void dispose() {
+        for (Sound s: loadedSounds.values()) {
+            s.dispose();
+        }
+    }
+
+    public static void preload() {
+        FileHandle fh = Gdx.files.internal("sound");
+        for (FileHandle file: fh.list()) {
+            String path = "sound/" + file.name();
+            Sound sound = Gdx.audio.newSound(file);
+            Gdx.app.log("SoundEffectHelper", "Preloading " + path);
+            loadedSounds.put(path, sound);
+        }
     }
 }
