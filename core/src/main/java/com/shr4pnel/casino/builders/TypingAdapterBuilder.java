10deb3a1b7532928f7816b852f304dbf2e892fe8
observer <admin@shr4pnel.com>
2025-04-27 19:53:39 +0100

    overhaul, more granular control over events and sound

--- core/src/main/java/com/shr4pnel/casino/builders/TypingAdapterBuilder.java
+++ core/src/main/java/com/shr4pnel/casino/builders/TypingAdapterBuilder.java
@@ -1,15 +1,26 @@
 package com.shr4pnel.casino.builders;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+
 import com.rafaskoberg.gdx.typinglabel.TypingAdapter;
+import com.rafaskoberg.gdx.typinglabel.TypingLabel;
 
 import com.shr4pnel.casino.audio.SoundEffect;
 import com.shr4pnel.casino.audio.SoundEffectHelper;
+import com.shr4pnel.casino.scene.SceneManager;
+import com.shr4pnel.casino.util.AsciiArt;
 
 public class TypingAdapterBuilder {
     private String path;
-    private float vol = 1f;
+    private float vol = 1;
+    private Window root;
+    private TypingLabel labelToAdd;
+    private int delayMillis = 2500;
+    private boolean stopSound = true;
+    private int height = 0;
+    private String eventName;
 
     public TypingAdapterBuilder setSound(String path) {
         this.path = path;
@@ -19,6 +30,32 @@ public class TypingAdapterBuilder {
     public TypingAdapterBuilder setSound(String path, float vol) {
         this.path = path;
         this.vol = vol;
+        return this;
+    }
+
+    public TypingAdapterBuilder dontStopSound() {
+        stopSound = false;
+        return this;
+    }
+
+    public TypingAdapterBuilder chainTypingLabel(TypingLabel tl, Window w) {
+        root = w;
+        labelToAdd = tl;
+        return this;
+    }
+
+    public TypingAdapterBuilder delay(int delayMillis) {
+        this.delayMillis = delayMillis;
+        return this;
+    }
+
+    public TypingAdapterBuilder addEvent(String s) {
+        eventName = s;
+        return this;
+    }
+
+    public TypingAdapterBuilder height(int height) {
+        this.height = height;
         return this;
     }
 
@@ -32,19 +69,53 @@ public class TypingAdapterBuilder {
 
         // time wasted here so far: 2 hours (fixing stupid scheduled thread executor bullshit)
         return new TypingAdapter() {
+            @Override
             public void end() {
                 sound.play();
-                Gdx.app.postRunnable(() -> {
-                    Actions.delay(5);
-                    Actions.run(() -> {
-                        endSound();
-                        sound.dispose();
-                    });
-                });
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(delayMillis);
+
+                        if (stopSound)
+                            endSound();
+
+                        if (root != null && labelToAdd != null) {
+                            root.add(labelToAdd).left().height(height).row();
+                        }
+
+
+                    } catch (InterruptedException e) {
+                        Gdx.app.log("TypingAdapterBuilder", "Interrupted while waiting on thread in build()", e);
+                    }
+                }).start();
             }
 
             private void endSound() {
                 sound.cancel();
+            }
+
+            @Override
+            public void event(String event) {
+                if (eventName.equals("start_art")) {
+                    new Thread(() -> {
+                        root.clear();
+                        for (Label l : AsciiArt.artAsTypingLabelArray()) {
+                            try {
+                                root.add(l).row();
+                                Thread.sleep(125);
+                            } catch (InterruptedException e) {
+                                Gdx.app.log("TypingAdapterBuilder", "Interrupted while waiting on thread in event()", e);
+                            }
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Gdx.app.log("TypingAdapterBuilder", "Interrupted while waiting on thread in event()", e);
+                        }
+                        root.clear();
+                        SceneManager.setActiveScene(SceneManager.scene.MENU);
+                    }).start();
+                }
             }
         };
     }
