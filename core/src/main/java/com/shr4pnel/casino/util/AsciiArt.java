a544ddc6f4fb71202519ddb6e326a0d40d758cca
observer <admin@shr4pnel.com>
2025-04-27 19:51:35 +0100

    add: asciiart, ascii tools for menus and the intro

--- core/src/main/java/com/shr4pnel/casino/util/AsciiArt.java
+++ core/src/main/java/com/shr4pnel/casino/util/AsciiArt.java
@@ -1,4 +1,36 @@
 package com.shr4pnel.casino.util;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.shr4pnel.casino.style.StyleManager;
+
+import java.util.ArrayList;
+
 public class AsciiArt {
+    public static final String[] cigarette = {
+        "                      ((\\",
+        "(              _  ,-_  \\ \\",
+        ")             / \\/  \\ \\ \\ \\",
+        "(            /)| \\/\\ \\ \\| |",
+        "`~()_______)___)\\ \\ \\ \\ \\ |",
+        "            |)\\ )  `' | | |",
+        "           /  /,          |",
+        "           |  |          /",
+        "           |  |         /",
+        "           \\           /",
+        "            \\         /",
+        "             )       /",
+        "            /       /",
+        "           /       /",
+        "                  /",
+        "     KILL EM ALL 2009",
+    };
+
+    public static Label[] artAsTypingLabelArray() {
+        ArrayList<Label> labelList = new ArrayList<>();
+        for (String s: cigarette) {
+            Label label = new Label(s, StyleManager.getSkin());
+            labelList.add(label);
+        }
+        return labelList.toArray(Label[]::new);
+    }
 }
