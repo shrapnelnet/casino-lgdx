6614a336bf538c41a510f0a62373bd60f8e1bd5b
observer <admin@shr4pnel.com>
2025-04-27 12:05:45 +0100

    add: sound cancellation, play sounds retaining ID (for later cancellation)

--- core/src/main/java/com/shr4pnel/casino/audio/SoundEffectHelper.java
+++ core/src/main/java/com/shr4pnel/casino/audio/SoundEffectHelper.java
@@ -3,7 +3,7 @@ package com.shr4pnel.casino.audio;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 
-public class SoundEffect {
+public class SoundEffectHelper {
     public static void play(String path) {
         Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
         sound.play(1.0f);
@@ -12,5 +12,20 @@ public class SoundEffect {
     public static void play(String path, float volume) {
         Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
         sound.play(volume);
+    }
+
+    public static long playWithID(String path) {
+        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        return sound.play(1.0f);
+    }
+
+    public static long playWithID(String path, float volume) {
+        Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+        return sound.play(volume);
+    }
+
+    public static void cancel(long id) {
+        Sound sound = Gdx.audio.newSound(Gdx.files.internal("sound/load.ogg"));
+        sound.stop(id);
     }
 }
