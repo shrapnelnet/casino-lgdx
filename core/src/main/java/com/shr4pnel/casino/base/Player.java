60a5314b7cc638ff9936beacd207b524a132c1bc
observer <admin@shr4pnel.com>
2025-04-27 19:47:52 +0100

    make player abstract, extend in specific games

--- core/src/main/java/com/shr4pnel/casino/base/Player.java
+++ core/src/main/java/com/shr4pnel/casino/base/Player.java
@@ -1,4 +1,21 @@
 package com.shr4pnel.casino.base;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Player {
+    protected List<Card> hand = new ArrayList<>();
+    protected boolean playerControlled;
+
+    public Player(boolean playerControlled) {
+        this.playerControlled = playerControlled;
+    }
+
+    public void add(Card card) {
+        hand.add(card);
+    }
+
+    public void add(List<Card> cards) {
+        hand.addAll(cards);
+    }
 }
--- core/src/main/java/com/shr4pnel/casino/blackjack/BlackjackPlayer.java
+++ core/src/main/java/com/shr4pnel/casino/blackjack/BlackjackPlayer.java
@@ -1,4 +1,40 @@
 package com.shr4pnel.casino.blackjack;
 
-public class BlackjackPlayer {
+import com.shr4pnel.casino.base.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlackjackPlayer extends Player {
+    private List<BlackjackCard> hand = new ArrayList<>();
+
+    public BlackjackPlayer(boolean playerControlled) {
+        super(playerControlled);
+    }
+
+    public boolean isBust() {
+        return false;
+    }
+
+    public int getHandValue() {
+        int handValue = 0;
+        List<BlackjackCard> aces = new ArrayList<>();
+        for (BlackjackCard c: hand) {
+            if (c.isAce()) {
+                aces.add(c);
+                continue;
+            }
+            handValue += c.getCardValue();
+        }
+
+        // aces are high or low. high used by default unless it would bust
+        for (BlackjackCard c: aces) {
+            if ((handValue + 11) > 21) {
+                handValue += 1;
+            } else {
+                handValue += 11;
+            }
+        }
+        return handValue;
+    }
 }
