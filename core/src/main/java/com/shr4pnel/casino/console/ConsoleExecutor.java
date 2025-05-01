fc2fcac6c1d23caeeca04b4f6ab716d1d2fc3cf6
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-01 15:07:15 +0100

    commands for post processing control, restart stub

--- core/src/main/java/com/shr4pnel/casino/console/ConsoleExecutor.java
+++ core/src/main/java/com/shr4pnel/casino/console/ConsoleExecutor.java
@@ -1,10 +1,22 @@
 package com.shr4pnel.casino.console;
 
+import com.shr4pnel.casino.Casino;
 import com.shr4pnel.casino.scene.SceneManager;
+import com.shr4pnel.casino.style.StyleManager;
 import com.strongjoshua.console.CommandExecutor;
 import com.strongjoshua.console.annotation.ConsoleDoc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConsoleExecutor extends CommandExecutor {
+    private Map<String, Boolean> stateStringToBool = new HashMap<>(2);
+
+    public ConsoleExecutor() {
+        stateStringToBool.put("on", true);
+        stateStringToBool.put("off", false);
+    }
+
     @ConsoleDoc(description = "Clears the console buffer")
     public void clear() {
         console.clear();
@@ -18,5 +30,19 @@ public class ConsoleExecutor extends CommandExecutor {
     @ConsoleDoc(description = "Gets the active scene")
     public void getscene() {
         console.log(SceneManager.getActiveScenePretty());
+    }
+
+    @ConsoleDoc(description = "Enable or disable post processing")
+    public void pp(String enabled) {
+        if (!(enabled.equals("on") || enabled.equals("off"))) {
+            console.log("Not one of ['on', 'off']");
+            return;
+        }
+        StyleManager.setPostProcessingEnabled(stateStringToBool.get(enabled));
+    }
+
+    @ConsoleDoc(description = "Reinitialize starting state, replay intro")
+    public void restart() {
+        SceneManager.setActiveScene(SceneManager.scene.INTRO);
     }
 }
