85049b3aa762f5c0aed4bd9cc2f0c838761b0041
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-01 15:02:52 +0100

    add post processing setters

--- core/src/main/java/com/shr4pnel/casino/style/StyleManager.java
+++ core/src/main/java/com/shr4pnel/casino/style/StyleManager.java
@@ -7,11 +7,25 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 public class StyleManager {
     private static final TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("skin/uiskin.atlas"));
     private static final Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+    private static boolean postProcessingEnabled = true;
+
     static {
         skin.addRegions(atlas);
     }
 
     public static Skin getSkin() {
         return StyleManager.skin;
+    }
+
+    public static boolean getPostProcessingEnabled() {
+        return postProcessingEnabled;
+    }
+
+    public static boolean getPostProcessingDisabled() {
+        return !getPostProcessingEnabled();
+    }
+
+    public static void setPostProcessingEnabled(boolean enabled) {
+        postProcessingEnabled = enabled;
     }
 }
