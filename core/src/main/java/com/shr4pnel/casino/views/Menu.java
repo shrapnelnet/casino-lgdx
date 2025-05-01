f5e8d9a54a9b50d9dfae1677a64eb8381b699ed1
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-01 15:03:37 +0100

    Add: menu, static methods for menu instantiation

--- core/src/main/java/com/shr4pnel/casino/views/Menu.java
+++ core/src/main/java/com/shr4pnel/casino/views/Menu.java
@@ -1,4 +1,66 @@
 package com.shr4pnel.casino.views;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.rafaskoberg.gdx.typinglabel.TypingLabel;
+
+import com.shr4pnel.casino.builders.LabelBuilder;
+import com.shr4pnel.casino.input.MenuState;
+import com.shr4pnel.casino.style.StyleManager;
+
 public class Menu {
+    private static final LabelBuilder labelBuilder = new LabelBuilder();
+    private static TextButton play, settings, quit;
+    private static TypingLabel label;
+    private static ButtonGroup<TextButton> textButtonGroup = new ButtonGroup<>();
+
+    public static Table get() {
+        Table root = new Table(StyleManager.getSkin());
+        Table labelRoot = new Table(StyleManager.getSkin());
+
+        label = labelBuilder
+            .start("{WIND}Casino-GDX{ENDWIND}")
+            .noDelay()
+            .build();
+
+        play = new TextButton("Play", StyleManager.getSkin(), "toggle");
+        settings = new TextButton("Settings", StyleManager.getSkin(), "toggle");
+        quit = new TextButton("Quit", StyleManager.getSkin(), "toggle");
+
+        play.setName("Play");
+        settings.setName("Settings");
+        quit.setName("Quit");
+
+        textButtonGroup.add(play, settings, quit);
+        textButtonGroup.setMaxCheckCount(1);
+        textButtonGroup.setMinCheckCount(1);
+        MenuState.setMenuButtonGroup(textButtonGroup);
+
+        play.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                return;
+            }
+        });
+
+        labelRoot.add(label).center();
+        root.background("window");
+        root.setSize(800, 450);
+
+        // without colspan, i stretch the play button out to 800 width
+        root.add(labelRoot).colspan(3).center().row();
+        root.add(play).pad(10);
+        root.add(settings).pad(10);
+        root.add(quit).pad(10).row();
+
+
+        return root;
+    }
+
+    public static void updateActiveButton() {
+        textButtonGroup.uncheckAll();
+        textButtonGroup.setChecked(MenuState.getActiveButton().getName());
+    }
 }
