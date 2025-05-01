78e06996ee90c195ce1b92d295d94c8c4ec40c58
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-01 15:07:48 +0100

    add: menuinputprocessor, control over inputs on menu

--- core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
+++ core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
@@ -1,4 +1,56 @@
 package com.shr4pnel.casino.input;
 
-public class MenuInputProcessor {
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+
+public class MenuInputProcessor implements InputProcessor {
+    @Override
+    public boolean keyDown(int keycode) {
+        return switch (keycode) {
+            case Keys.LEFT -> MenuState.left();
+            case Keys.RIGHT -> MenuState.right();
+            case Keys.Z, Keys.ENTER, Keys.NUMPAD_ENTER -> MenuState.enter();
+            default -> false;
+        };
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
 }
