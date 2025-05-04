c29ade5bae404c1fe6a1a41bf853d89b26c92e13
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-04 17:39:35 +0100

    don't use MenuState, use ButtonGroupManager

--- core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
+++ core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
@@ -2,14 +2,18 @@ package com.shr4pnel.casino.input;
 
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
+import com.shr4pnel.casino.scene.Menu;
+import com.shr4pnel.casino.util.ButtonGroupManager;
 
 public class MenuInputProcessor implements InputProcessor {
+    private final ButtonGroupManager buttonGroupManager = Menu.getButtonGroupManager();
+
     @Override
     public boolean keyDown(int keycode) {
         return switch (keycode) {
-            case Keys.LEFT -> MenuState.left();
-            case Keys.RIGHT -> MenuState.right();
-            case Keys.Z, Keys.ENTER, Keys.NUMPAD_ENTER -> MenuState.enter();
+            case Keys.LEFT -> buttonGroupManager.left();
+            case Keys.RIGHT -> buttonGroupManager.right();
+            case Keys.Z, Keys.ENTER, Keys.NUMPAD_ENTER -> buttonGroupManager.enter();
             default -> false;
         };
     }
