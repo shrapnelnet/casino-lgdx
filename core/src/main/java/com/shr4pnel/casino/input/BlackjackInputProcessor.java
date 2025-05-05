f3f543ba7565bcafb692e2a95d4e9b48cf5a2756
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-05 10:40:50 +0100

    use inputadapter over inputprocessor

--- core/src/main/java/com/shr4pnel/casino/input/BlackjackInputProcessor.java
+++ core/src/main/java/com/shr4pnel/casino/input/BlackjackInputProcessor.java
@@ -1,11 +1,11 @@
 package com.shr4pnel.casino.input;
 
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.InputAdapter;
 import com.shr4pnel.casino.scene.Blackjack;
 import com.shr4pnel.casino.util.ButtonGroupManager;
 
-public class BlackjackInputProcessor implements InputProcessor {
+public class BlackjackInputProcessor extends InputAdapter {
     private final ButtonGroupManager buttonGroupManager = Blackjack.getButtonGroupManager();
 
     @Override
@@ -16,45 +16,5 @@ public class BlackjackInputProcessor implements InputProcessor {
             case Keys.Z, Keys.ENTER, Keys.NUMPAD_ENTER -> buttonGroupManager.enter();
             default -> false;
         };
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
     }
 }
--- core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
+++ core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
@@ -1,11 +1,11 @@
 package com.shr4pnel.casino.input;
 
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.InputAdapter;
 import com.shr4pnel.casino.scene.Menu;
 import com.shr4pnel.casino.util.ButtonGroupManager;
 
-public class MenuInputProcessor implements InputProcessor {
+public class MenuInputProcessor extends InputAdapter {
     private final ButtonGroupManager buttonGroupManager = Menu.getButtonGroupManager();
 
     @Override
@@ -16,45 +16,5 @@ public class MenuInputProcessor implements InputProcessor {
             case Keys.Z, Keys.ENTER, Keys.NUMPAD_ENTER -> buttonGroupManager.enter();
             default -> false;
         };
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
     }
 }
