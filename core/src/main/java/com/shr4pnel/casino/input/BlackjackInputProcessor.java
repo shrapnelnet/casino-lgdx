de2d731ab5b0e492556efa93b799b5c25568ee96
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-07 14:42:23 +0100

    inherit keydown rather than overriding

--- core/src/main/java/com/shr4pnel/casino/input/BlackjackInputProcessor.java
+++ core/src/main/java/com/shr4pnel/casino/input/BlackjackInputProcessor.java
@@ -1,20 +1,12 @@
 package com.shr4pnel.casino.input;
 
-import com.badlogic.gdx.Input.Keys;
 import com.shr4pnel.casino.scene.ManagedButtonScene;
 
+/**
+ * stub, in case we need more options later
+ */
 public class BlackjackInputProcessor extends InputProcessor {
     public BlackjackInputProcessor(ManagedButtonScene s) {
         super(s);
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return switch (keycode) {
-            case Keys.LEFT -> manager.left();
-            case Keys.RIGHT -> manager.right();
-            case Keys.Z, Keys.ENTER, Keys.NUMPAD_ENTER -> manager.enter();
-            default -> false;
-        };
     }
 }
--- core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
+++ core/src/main/java/com/shr4pnel/casino/input/MenuInputProcessor.java
@@ -1,20 +1,9 @@
 package com.shr4pnel.casino.input;
 
-import com.badlogic.gdx.Input.Keys;
 import com.shr4pnel.casino.scene.ManagedButtonScene;
 
 public class MenuInputProcessor extends InputProcessor {
     public MenuInputProcessor(ManagedButtonScene s) {
         super(s);
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return switch (keycode) {
-            case Keys.LEFT -> manager.left();
-            case Keys.RIGHT -> manager.right();
-            case Keys.Z, Keys.ENTER, Keys.NUMPAD_ENTER -> manager.enter();
-            default -> false;
-        };
     }
 }
