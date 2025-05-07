90cff0618dacb73bf0ecf3b781dd6823c926d158
shrapnelnet <shrapnelnet@protonmail.com>
2025-05-07 13:10:10 +0100

    add methods to handle bet incrementing

--- core/src/main/java/com/shr4pnel/casino/util/ButtonGroupManager.java
+++ core/src/main/java/com/shr4pnel/casino/util/ButtonGroupManager.java
@@ -3,18 +3,34 @@ package com.shr4pnel.casino.util;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.shr4pnel.casino.Casino;
+import com.shr4pnel.casino.base.Game;
+import com.shr4pnel.casino.base.Player;
+import com.shr4pnel.casino.scene.Blackjack;
+import com.shr4pnel.casino.scene.ManagedButtonGame;
 import com.shr4pnel.casino.scene.SceneManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ButtonGroupManager {
     private final ButtonGroup<TextButton> menuButtonGroup = new ButtonGroup<>();
     private TextButton activeButton;
     private ButtonGroupListener listener;
+    private final Map<String, Long> incrementStringToLong = new HashMap<>();
 
     public ButtonGroupManager(TextButton... t) {
         setMenuButtonGroup(t);
+        incrementStringToLong.put("---", -100L);
+        incrementStringToLong.put("--", -10L);
+        incrementStringToLong.put("-", -1L);
+        incrementStringToLong.put("+", 1L);
+        incrementStringToLong.put("++", 10L);
+        incrementStringToLong.put("+++", 100L);
     }
 
     public void setMenuButtonGroup(TextButton... t) {
+        menuButtonGroup.clear();
         menuButtonGroup.add(t);
         // bugfix: no active button is set if we don't call setActiveButton at construction time (CTD)
         setActiveButton(menuButtonGroup.getChecked());
@@ -82,13 +98,30 @@ public class ButtonGroupManager {
                 SceneManager.setActiveScene(SceneManager.Scene.BLACKJACK);
                 yield true;
             }
+
             case "Quit" -> {
                 Gdx.app.exit();
                 yield true;
             }
+
             case "Settings" -> true;
+
+            case "---", "--", "-", "+", "++", "+++" -> incrementBet(activeButton.getName());
+
             default -> false;
         };
+    }
+
+    private boolean incrementBet(String name) {
+        Casino c = Casino.getInstance();
+        ManagedButtonGame scene = c.getGameInstance(SceneManager.getActiveScene());
+        Game g = scene.getGameInstance();
+        Player p = g.getPlayer();
+        p.incrementBet(incrementStringToLong.get(name));
+
+        if (scene instanceof Blackjack)
+            ((Blackjack) scene).updateChipDisplay();
+        return true;
     }
 
     public void setListener(ButtonGroupListener listener) {
