f57516d00deca61d87da5da33f1e097bc1cb6808
Rilpp <rileymcp1@gmail.com>
2025-05-15 00:44:17 +0100

    add rouletteBetType enum, bet type hash map

--- core/src/main/java/com/shr4pnel/casino/roulette/RouletteGame.java
+++ core/src/main/java/com/shr4pnel/casino/roulette/RouletteGame.java
@@ -2,9 +2,32 @@ package com.shr4pnel.casino.roulette;
 
 import com.shr4pnel.casino.base.Game;
 import com.shr4pnel.casino.base.Player;
+import com.shr4pnel.casino.blackjack.BlackjackGame;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class RouletteGame extends Game {
+    private Map<RouletteGame.rouletteBetType, Integer> betTypeToMult = new HashMap<>();
+
+    public RouletteGame() {
+        betTypeToMult.put(rouletteBetType.SINGLE, 35);
+        betTypeToMult.put(RouletteGame.rouletteBetType.EVEN, 1);
+        betTypeToMult.put(RouletteGame.rouletteBetType.ODD, 1);
+        betTypeToMult.put(RouletteGame.rouletteBetType.BLACK, 1);
+        betTypeToMult.put(RouletteGame.rouletteBetType.RED, 1);
+    }
+
+
+    public enum rouletteBetType {
+        SINGLE,
+        EVEN,
+        ODD,
+        BLACK,
+        RED
+    }
+
     @Override
     public RoulettePlayer getPlayer() {
         return (RoulettePlayer) player;
